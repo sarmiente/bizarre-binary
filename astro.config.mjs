101d860beb13010cf37ffd0637bf56990e830346
Carlos Sarmiento <carlosarmientopinto@gmail.com>
2024-10-17 12:08:15 -0500

    debugging

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify"
+import cloudflare from "@astrojs/cloudflare"
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import simpleStackForm from "simple-stack-form";
@@ -23,5 +23,5 @@ export default defineConfig({
     simpleStackForm(),
   ],
   output: "hybrid",
-  adapter: netlify()
+  adapter: cloudflare()
 });
